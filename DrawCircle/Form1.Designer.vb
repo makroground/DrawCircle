dd0ba0b1503c452e13b1651d2e5f4aad78521f0b
makroground <r.bressler@makroground.de>
2015-10-01 22:17:57 +0200

    Erweiterungen des Vorhanden Codes um: - kleiner werdenden Kreis (und Mittelpunkt am selbem Fleck bleibend) - Punkteanzeige und Meldung über Ende des Spiels

--- DrawCircle/Form1.Designer.vb
+++ DrawCircle/Form1.Designer.vb
@@ -25,29 +25,73 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(708, 392)
+        Me.Button1.Location = New System.Drawing.Point(1168, 12)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(217, 40)
+        Me.Button1.Size = New System.Drawing.Size(99, 40)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Zeichne einen Kreis"
+        Me.Button1.Text = "Start"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Timer1
         '
+        Me.Timer1.Interval = 80
         '
-        'Button2
+        'Label1
         '
-        Me.Button2.Location = New System.Drawing.Point(708, 454)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(217, 40)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Lösche den Kreis"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(975, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Fails:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(1045, 10)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(19, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(1081, 11)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "/10"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(964, 40)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Points:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(1045, 40)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(19, 20)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "0"
         '
         'Form1
         '
@@ -55,15 +99,25 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Beige
         Me.ClientSize = New System.Drawing.Size(1279, 712)
-        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "DrawCircle - Game"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Timer1 As Timer
-    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
 End Class
--- DrawCircle/Form1.vb
+++ DrawCircle/Form1.vb
@@ -3,10 +3,17 @@
 Public Class Form1
 
     Dim graphics As Graphics
-    Dim circleRadius As Integer = 50
+    Dim circleRadius As Integer = 30
+    Dim currentRadius As Integer = circleRadius
+    Dim circlePos As Point
     Dim cPresent = False
 
+    Dim speed As Integer = 1
     Dim level As Integer
+    Dim points As Integer
+    Dim failes As Integer
+    Dim maxFailes As Integer = 10
+    Dim catched As Boolean = False
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Timer1.Start()
@@ -18,41 +25,74 @@ Public Class Form1
 
     Private Sub DrawACircle(ByRef center As Point, ByVal radius As Integer)
 
+        Dim xCenter As Integer = center.X - radius
+        Dim yCenter As Integer = center.Y - radius
+        Dim diameter As Integer = radius * 2
         graphics = Me.CreateGraphics
 
-        'Dim pn As New Pen(Color.Blue)
+        Dim pn As New Pen(Color.Red)
         Dim gp As GraphicsPath = New GraphicsPath
-        gp.AddEllipse(50, 50, 30, 30)
 
-        Dim rect As New Rectangle(center.X / 2, center.Y / 2, radius * 2, radius * 2)
+
+        gp.AddEllipse(xCenter, yCenter, diameter, diameter)
+
+        Dim rect As New Rectangle(xCenter, yCenter, diameter, diameter)
         'graphics.DrawEllipse(pn, rect)
         graphics.FillEllipse(Brushes.Black, rect)
+        'graphics.DrawLine(pn, New Point(center.X - radius, center.Y), New Point(center.X + radius, center.Y))
+        'graphics.DrawLine(pn, New Point(center.X, center.Y - radius), New Point(center.X, center.Y + radius))
         graphics.SetClip(gp, CombineMode.Replace)
+        'Debug.WriteLine("x: " & xCenter & "y: " & yCenter)
 
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        If (cPresent) Then
+        If (failes >= maxFailes) Then
             If (graphics.IsClipEmpty = False) Then
                 graphics.Clear(Color.Beige)
             End If
             cPresent = False
+            MsgBox("Looooosed!!!")
+            Timer1.Stop()
+        ElseIf (catched) Then
+            If (graphics.IsClipEmpty = False) Then
+                graphics.Clear(Color.Beige)
+            End If
+            cPresent = False
+            currentRadius = circleRadius
+            points += 100
+            Label4.Text = points.ToString
+            catched = False
+        End If
+        If (cPresent) Then
+            If (currentRadius > 0) Then
+                currentRadius -= speed
+                If (graphics.IsClipEmpty = False) Then
+                    graphics.Clear(Color.Beige)
+                End If
+                DrawACircle(circlePos, currentRadius)
+            Else
+                failes += 1
+                Label2.Text = failes.ToString
+                If (graphics.IsClipEmpty = False) Then
+                    graphics.Clear(Color.Beige)
+                End If
+                currentRadius = circleRadius
+                cPresent = False
+            End If
         Else
-            DrawACircle(New Point(100, 100), 15)
+            circlePos = New Point(200, 200)
+            DrawACircle(circlePos, circleRadius)
             cPresent = True
         End If
     End Sub
 
     Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
         If (graphics.IsVisible(e.X, e.Y)) Then
-            MsgBox("Circle was clicked")
+            'MsgBox("Circle was clicked")
+            catched = True
         End If
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        'If (graphics.IsClipEmpty = False) Then
-        'graphics.Clear(Color.White)
-        'End If
-        Timer1.Stop()
-    End Sub
 End Class
+
