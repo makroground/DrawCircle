26800de25c163597d198a541e5660cdb32c997e9
makroground <r.bressler@makroground.de>
2015-10-11 11:57:12 +0200

    Anpassung der Funktion, Kreis wächst anstatt zu schrumpfen, Start Button wird nach dem Starten ausgeblendet, Kreis wird zufällig innerhalb des Spielfeldes gezeichnet, Begonnen der Kommentierung des Quellcodes

--- DrawCircle/Form1.Designer.vb
+++ DrawCircle/Form1.Designer.vb
@@ -30,13 +30,23 @@ Partial Class Form1
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(1168, 12)
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(1088, 19)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(99, 40)
+        Me.Button1.Size = New System.Drawing.Size(138, 52)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Start"
         Me.Button1.UseVisualStyleBackColor = True
@@ -47,51 +57,115 @@ Partial Class Form1
         '
         'Label1
         '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(975, 11)
+        Me.Label1.Location = New System.Drawing.Point(24, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(46, 20)
+        Me.Label1.Size = New System.Drawing.Size(59, 25)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Fails:"
         '
         'Label2
         '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(1045, 10)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(99, 9)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(19, 20)
+        Me.Label2.Size = New System.Drawing.Size(24, 25)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "0"
         '
         'Label3
         '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(1081, 11)
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(135, 9)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(31, 20)
+        Me.Label3.Size = New System.Drawing.Size(40, 25)
         Me.Label3.TabIndex = 4
         Me.Label3.Text = "/10"
         '
         'Label4
         '
+        Me.Label4.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(964, 40)
+        Me.Label4.Location = New System.Drawing.Point(33, 9)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(57, 20)
+        Me.Label4.Size = New System.Drawing.Size(72, 25)
         Me.Label4.TabIndex = 5
         Me.Label4.Text = "Points:"
         '
         'Label5
         '
+        Me.Label5.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(1045, 40)
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(115, 9)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(19, 20)
+        Me.Label5.Size = New System.Drawing.Size(24, 25)
         Me.Label5.TabIndex = 6
         Me.Label5.Text = "0"
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.Location = New System.Drawing.Point(773, 24)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(207, 40)
+        Me.Panel1.TabIndex = 7
+        '
+        'Panel2
+        '
+        Me.Panel2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel2.Location = New System.Drawing.Point(520, 24)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(207, 40)
+        Me.Panel2.TabIndex = 8
+        '
+        'Panel3
+        '
+        Me.Panel3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Controls.Add(Me.Label6)
+        Me.Panel3.Controls.Add(Me.Label7)
+        Me.Panel3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel3.Location = New System.Drawing.Point(266, 24)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(207, 40)
+        Me.Panel3.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(40, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(65, 25)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Level:"
+        '
+        'Label7
+        '
+        Me.Label7.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(122, 9)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(24, 25)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "1"
         '
         'Form1
         '
@@ -99,17 +173,20 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Beige
         Me.ClientSize = New System.Drawing.Size(1279, 712)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.ShowIcon = False
         Me.Text = "DrawCircle - Game"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -120,4 +197,9 @@ Partial Class Form1
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
 End Class
--- DrawCircle/Form1.vb
+++ DrawCircle/Form1.vb
@@ -2,51 +2,56 @@
 
 Public Class Form1
 
-    Dim graphics As Graphics
-    Dim circleRadius As Integer = 30
-    Dim currentRadius As Integer = circleRadius
-    Dim circlePos As Point
-    Dim cPresent = False
+    Dim graphics As Graphics                                                                                        ' Graphics Objekt, welches den Kreis darstellt
+    Dim maxRadius As Integer = 30                                                                                   ' Maximale Größe des Kreises (Wird nur bei einem "Fail" dargestellt)
+    Dim currentRadius As Integer = 0                                                                                ' Beinhaltet die aktuelle Größe des Kreises, der von 0 bis 'maxRadius' variiert
+    Dim circlePos As Point                                                                                          ' Aktuelle Koordinaten des Kreises innerhalb der Form (begrenzter Zufallswert)
+    Dim cPresent = False                                                                                            ' Gibt an, ob sich zum Zeitpunkt ein Kreis auf der Form befindet
 
-    Dim speed As Integer = 1
-    Dim level As Integer
-    Dim points As Integer
-    Dim failes As Integer
-    Dim maxFailes As Integer = 10
-    Dim catched As Boolean = False
+    Dim speed As Integer = 1                                                                                        ' Schwellwert für das Wachsen des Kreises (bezogen auf den Radius) pro Timerdurchlauf
+    Dim level As Integer                                                                                            ' Level im Spiel
+    Dim points As Integer                                                                                           ' Spielpunkte pro Level
+    Dim failes As Integer                                                                                           ' Quantität der nicht getroffenen/angeklickten Kreise
+    Dim maxFailes As Integer = 10                                                                                   ' Maximal mögliche 'failes' bis zum Verlieren des Spieles
+    Dim catched As Boolean = False                                                                                  ' Gibt an, ob der aktuelle Kreis getroffen wurde
+
+    Dim wallTop = 0                                                                                                 ' Platzhalter für den oberen Beginn des Spielfeldes
+    Dim wallBottom = 0                                                                                              ' Platzhalter für das untere Ende des Spielfeldes
+    Dim wallStart = 0                                                                                               ' Platzhalter für den linken Beginn des Spielfeldes
+    Dim wallStop = 0                                                                                                ' Platzhalter für das rechte Ende des Spielfeldes
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Timer1.Start()
-        'DrawACircle(New Point(100, 100), 30)
+
+        Timer1.Start()                                                                                              ' Spiel starten
+        Button1.Visible = False                                                                                     ' Start Knopf ausblenden, solange das Spiel läuft
+
     End Sub
 
-    Private Sub Draw(e As PaintEventArgs)
-    End Sub
 
     Private Sub DrawACircle(ByRef center As Point, ByVal radius As Integer)
 
+        graphics = Me.CreateGraphics
         Dim xCenter As Integer = center.X - radius
         Dim yCenter As Integer = center.Y - radius
         Dim diameter As Integer = radius * 2
-        graphics = Me.CreateGraphics
 
-        Dim pn As New Pen(Color.Red)
+        'Dim pn As New Pen(Color.Red)
         Dim gp As GraphicsPath = New GraphicsPath
-
-
         gp.AddEllipse(xCenter, yCenter, diameter, diameter)
+        graphics.SetClip(gp, CombineMode.Replace)
 
         Dim rect As New Rectangle(xCenter, yCenter, diameter, diameter)
         'graphics.DrawEllipse(pn, rect)
         graphics.FillEllipse(Brushes.Black, rect)
-        'graphics.DrawLine(pn, New Point(center.X - radius, center.Y), New Point(center.X + radius, center.Y))
-        'graphics.DrawLine(pn, New Point(center.X, center.Y - radius), New Point(center.X, center.Y + radius))
-        graphics.SetClip(gp, CombineMode.Replace)
-        'Debug.WriteLine("x: " & xCenter & "y: " & yCenter)
 
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        wallTop = 100
+        wallBottom = Me.Height - 100
+        wallStart = 100
+        wallStop = Me.Width - 100
+
         If (failes >= maxFailes) Then
             If (graphics.IsClipEmpty = False) Then
                 graphics.Clear(Color.Beige)
@@ -59,38 +64,44 @@ Public Class Form1
                 graphics.Clear(Color.Beige)
             End If
             cPresent = False
-            currentRadius = circleRadius
+            currentRadius = 0
             points += 100
-            Label4.Text = points.ToString
+            Label5.Text = points.ToString
             catched = False
-        End If
-        If (cPresent) Then
-            If (currentRadius > 0) Then
-                currentRadius -= speed
-                If (graphics.IsClipEmpty = False) Then
-                    graphics.Clear(Color.Beige)
-                End If
-                DrawACircle(circlePos, currentRadius)
-            Else
-                failes += 1
-                Label2.Text = failes.ToString
-                If (graphics.IsClipEmpty = False) Then
-                    graphics.Clear(Color.Beige)
-                End If
-                currentRadius = circleRadius
-                cPresent = False
-            End If
         Else
-            circlePos = New Point(200, 200)
-            DrawACircle(circlePos, circleRadius)
-            cPresent = True
+            If (cPresent) Then
+                If (currentRadius <= maxRadius) Then
+                    currentRadius += speed
+                    If (graphics.IsClipEmpty = False) Then
+                        graphics.Clear(Color.Beige)
+                    End If
+                    DrawACircle(circlePos, currentRadius)
+                Else
+                    failes += 1
+                    Label2.Text = failes.ToString
+                    If (graphics.IsClipEmpty = False) Then
+                        graphics.Clear(Color.Beige)
+                    End If
+                    currentRadius = 0
+                    cPresent = False
+                End If
+            Else
+                Dim rndX As Integer = CInt(Int(((wallStop - wallStart + 1) * Rnd()) + wallStart))
+                Dim rndY As Integer = CInt(Int(((wallBottom - wallTop + 1) * Rnd()) + wallTop))
+                circlePos = New Point(rndX, rndY)
+                DrawACircle(circlePos, currentRadius)
+                cPresent = True
+            End If
         End If
+
     End Sub
 
     Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
-        If (graphics.IsVisible(e.X, e.Y)) Then
-            'MsgBox("Circle was clicked")
-            catched = True
+        If Not graphics Is Nothing Then
+            If graphics.IsVisible(e.X, e.Y) Then
+                'MsgBox("Circle was clicked")
+                catched = True
+            End If
         End If
     End Sub
 
